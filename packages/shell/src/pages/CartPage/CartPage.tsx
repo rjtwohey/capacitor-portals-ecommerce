c8ddcd47f714bb227b03e8d13c7a71483ef5a4a9
Ryan Twohey <rtwohey@ionic.io>
2026-06-05 10:16:43 -0400

    title page change live update test

--- packages/shell/src/pages/CartPage/CartPage.tsx
+++ packages/shell/src/pages/CartPage/CartPage.tsx
@@ -54,7 +54,7 @@ const CartPage: React.FC<RouteComponentProps<{}>> = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense" translucent={true}>
           <IonToolbar>
-            <IonTitle size="large">Cart</IonTitle>
+            <IonTitle size="large">Cart 2</IonTitle>
           </IonToolbar>
         </IonHeader>
         {!cart || cart.basket?.length === 0 ? (
